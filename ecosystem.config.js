0bc677a12e2ee36e731406995f2e456b83abd33a
Dorian Bayart <dorianbayart@users.noreply.github.com>
2021-05-14 22:23:35 +0200

    Initialize ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  apps : [{
+    name: 'DexPairs - Back',
+    script: 'back.js',
+    watch: 'back.js',
+    //ignore_watch : ["node_modules", "file"]
+    env_production: {
+      NODE_ENV: "production",
+      PORT: 3000,
+    }
+  }, {
+    name: 'DexPairs - Front',
+    script: 'front.js',
+    watch: 'front.js',
+    //ignore_watch : ["node_modules", "file"],
+    //instances: 1,
+    //exec_mode: "cluster",
+    increment_var : 'PORT',
+    env_production: {
+      NODE_ENV: "production",
+      PORT: 3001,
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'dexpairs',
+      host : '185.212.226.82',
+      ref  : 'origin/main',
+      repo : 'git@github.com:dorianbayart/DexPairs.git',
+      path : '/home/dexpairs/prod',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js --env production',
+      'pre-setup': '',
+      env_production: {
+        NODE_ENV: "production"
+      }
+    }
+  }
+};
