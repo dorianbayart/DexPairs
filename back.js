9971f526e51b2f2d22f77044e5aedb8f67d72779
Dorian Bayart <dorianbayart@users.noreply.github.com>
2021-05-20 15:06:58 +0200

    Big update - Start implementing Uniswap back

--- back.js
+++ back.js
@@ -35,21 +35,30 @@ console.log(dir_home)
 /* DexPairs */
 
 
+// Pancake data
 let tokens_list = {}
 let top_tokens = {}
 let tokens_data = {}
 let tokens_charts = {}
 
+// Uniswap data
+let uniswap_list = {}
+let uniswap_top = {}
+let uniswap_data = {}
+let uniswap_charts = {}
+
+
 
 // Utils
-async function get(url) {
+async function get(url, request = null) {
   return new Promise((resolve, reject) => {
-    fetch(url)
+    fetch(url, { body: request })
     .then((response) => response.json())
     .then(resolve)
     .catch(reject)
   });
 }
+
 
 // Get Pancake's top
 async function getTopTokens() {
@@ -59,9 +68,33 @@ async function getTopPairs() {
   return await get("https://api.pancakeswap.info/api/v2/pairs")
 }
 
+// Get Uniswap's top
+const uniswap_request = {
+  tokens(
+    orderBy: tradeVolumeUSD,
+    orderDirection: desc,
+    where: {totalLiquidity_gt: "10"} // filter shit tokens
+  ) {
+    id
+    name
+    symbol
+    derivedETH
+    tradeVolumeUSD
+    txCount
+    totalLiquidity
+  }
+  bundle(id: "1" ) {
+    ethPrice
+  }
+}
+// Use TheGraph API - https://thegraph.com/explorer/subgraph/uniswap/uniswap-v2
+async function getUniswapTopTokens() {
+  return await get("https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2", JSON.stringify(uniswap_request))
+}
 
 
-// Program
+
+// Program - Pancake
 async function launch() {
   let tokens_data_file = {}
   let tokens_charts_file = {}
@@ -77,7 +110,7 @@ async function launch() {
 
 
   // get data from PancakeSwap
-  top = await getTopTokens()
+  const top = await getTopTokens()
 
 
 
@@ -142,7 +175,7 @@ async function launch() {
           tokens_charts[address].chart_4h.push({
             t: tx,
             p: vx,
-            //p_BNB: price_BNB
+            // TODO Remove following - used to debug
             val1: val1,
             val2: val2,
             a: a,
@@ -184,8 +217,6 @@ async function launch() {
 
 
 
-
-
   /* Store files */
 
   // Update the tokens list
@@ -217,11 +248,157 @@ async function launch() {
 }
 
 
+// Program - Uniswap
+async function launchUniswap() {
+  let uniswap_data_file = {}
+  let uniswap_charts_file = {}
+  try {
+    uniswap_data_file = require(path.join(dir_home, 'uniswap-simple.json'))
+    uniswap_charts_file = require(path.join(dir_home, 'uniswap-charts.json'))
+  } catch(error) {
+    console.log(error)
+  }
+
+  uniswap_data = uniswap_data_file
+  uniswap_charts = uniswap_charts_file
+
+
+  // get data from Uniswap
+  const top = await getUniswapTopTokens()
+
+
+
+  const time = Date.now()
+  const tokens = top.data.tokens
+  
+  const eth_price = top.data.bundle.ethPrice
+
+  tokens.forEach(token => {
+      const address = token.id
+      const symbol = token.symbol
+      const name = token.name
+      const price_ETH = token.derivedETH
+      const price = price_ETH * eth_price
+
+      // create Uniswap list
+      uniswap_list[address] = symbol
+
+
+      // update Uniswap simple data
+      uniswap_data[address] = {
+        s: symbol,
+        n: name,
+        p: price,
+        t: time
+      }
+
+      // update Uniswap charts
+      //
+      if(uniswap_charts[address]) {
+        if(uniswap_charts[address].chart_often[uniswap_charts[address].chart_often.length-1]['t'] < time) {
+          uniswap_charts[address].chart_often.push({
+            t: time,
+            p: price,
+          })
+          uniswap_charts[address].chart_often = uniswap_charts[address].chart_often.slice(-60)
+        }
+      } else {
+        uniswap_charts[address] = {
+          s: symbol,
+          n: name,
+          chart_often: [{
+            t: time,
+            p: price,
+          }]
+        }
+      }
+      if(uniswap_charts[address].chart_4h) {
+        if((time - uniswap_charts[address].chart_4h[uniswap_charts[address].chart_4h.length-1]['t']) > 14400000) {
+          const val1 = uniswap_charts[address].chart_often[uniswap_charts[address].chart_often.length-2]
+          const val2 = uniswap_charts[address].chart_often[uniswap_charts[address].chart_often.length-1]
+          const v1 = val1.price
+          const t1 = val1.t
+          const v2 = val2.price
+          const t2 = val2.t
+          const a = (v2 - v1) / (t2 - t1)
+          const b = v1 - a * t1
+          const tx = uniswap_charts[address].chart_4h[uniswap_charts[address].chart_4h.length-1]['t'] + 14400000
+          const vx = a * tx + b
+          uniswap_charts[address].chart_4h.push({
+            t: tx,
+            p: vx,
+            // TODO Remove following - used to debug
+            val1: val1,
+            val2: val2,
+            a: a,
+            b: b
+          })
+          uniswap_charts[address].chart_4h = uniswap_charts[address].chart_4h.slice(-60)
+        }
+      } else {
+        uniswap_charts[address].chart_4h = [{
+          t: time,
+          p: price,
+        }]
+      }
+  }
+
+
+  // build Top 25 list of Uniswap
+  uniswap_top = {}
+  for (var i = 0; i < 25; i++) {
+    const token = tokens[i]
+    const address = token.id
+    const symbol = token.symbol
+    const name = token.name
+    const price_ETH = token.derivedETH
+    const price = price_ETH * eth_price
+
+    uniswap_top[address] = {
+      s: symbol,
+      n: name,
+      p: price,
+      chart: uniswap_charts[token].chart_often
+    }
+  }
+
+
+  /* Store files */
+
+  // Update the Uniswap list
+  let pathFile = path.join(dir_home, 'uniswap.json')
+  writeFile( pathFile, JSON.stringify( uniswap_list ), "utf8", (err) => {
+    if (err) throw err;
+  });
+
+  // Update the Uniswap top 25
+  pathFile = path.join(dir_home, 'uniswap-top.json')
+  writeFile( pathFile, JSON.stringify( uniswap_top ), "utf8", (err) => {
+    if (err) throw err;
+  });
+
+  // Update the Uniswap simple data
+  pathFile = path.join(dir_home, 'uniswap-simple.json')
+  writeFile( pathFile, JSON.stringify( uniswap_data ), "utf8", (err) => {
+    if (err) throw err;
+  });
+
+  // Update the Uniswap charts
+  pathFile = path.join(dir_home, 'uniswap-charts.json')
+  writeFile( pathFile, JSON.stringify( uniswap_charts ), "utf8", (err) => {
+    if (err) throw err;
+  });
+
+  // loop
+  setTimeout(function(){ launchUniswap() }, 60000);
+}
+
 
 
 
 /* MAIN */
 launch()
+launchUniswap()
 
 
 
@@ -232,10 +409,16 @@ launch()
 const port = process.env.PORT || 3000
 const app = express()
 
-app.get('/list', (req, res) => res.json(tokens_list))
-app.get('/top', (req, res) => res.json(top_tokens))
-app.get('/simple', (req, res) => res.json(tokens_data))
-app.get('/charts', (req, res) => res.json(tokens_charts))
+// Pancake URLs
+app.get('/list/pancake', (req, res) => res.json(tokens_list))
+app.get('/top/pancake', (req, res) => res.json(top_tokens))
+app.get('/simple/pancake', (req, res) => res.json(tokens_data))
+app.get('/charts/pancake', (req, res) => res.json(tokens_charts))
+// Uniswap URLs
+app.get('/list/uniswap', (req, res) => res.json(uniswap_list))
+app.get('/top/uniswap', (req, res) => res.json(uniswap_top))
+app.get('/simple/uniswap', (req, res) => res.json(uniswap_data))
+app.get('/charts/uniswap', (req, res) => res.json(uniswap_charts))
 
 app.listen(port, () => console.log(`Backend start at ${port}`))
 
