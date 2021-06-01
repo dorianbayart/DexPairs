20ec05adcbb2d53b8368b363c642d490d101afaf
Dorian Bayart <dorianbayart@users.noreply.github.com>
2021-06-01 12:01:39 +0200

    Add support of Sushiswap on Polygon

--- back.js
+++ back.js
@@ -42,17 +42,23 @@ const WEEK = 604800000 // 1 week
 /* DexPairs */
 
 
-// Pancake data
+// Uniswap data - Ehtereum
+let uniswap_list = {}
+let uniswap_top = {}
+let uniswap_data = {}
+let uniswap_charts = {}
+
+// Pancake data - BinanceSmartChain
 let tokens_list = {}
 let top_tokens = {}
 let tokens_data = {}
 let tokens_charts = {}
 
-// Uniswap data
-let uniswap_list = {}
-let uniswap_top = {}
-let uniswap_data = {}
-let uniswap_charts = {}
+// Sushiswap data - Polygon/Matic
+let sushiswap_list = {}
+let sushiswap_top = {}
+let sushiswap_data = {}
+let sushiswap_charts = {}
 
 
 
@@ -106,6 +112,27 @@ query
 // Use TheGraph API - https://thegraph.com/explorer/subgraph/uniswap/uniswap-v2
 async function getUniswapTopTokens() {
   return await get("https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2", uniswap_request)
+}
+
+// Get Sushiswap's top
+const sushiswap_request = `
+query
+  {
+    tokens(first: 1000, orderBy: volumeUSD, orderDirection: desc, where: { liquidity_gt: "10" } ) {
+      id
+      name
+      symbol
+      derivedETH
+    }
+    bundle(id: "1" ) {
+      ethPrice
+    }
+  }
+`
+
+// Use TheGraph API - https://thegraph.com/explorer/subgraph/sushiswap/matic-exchange
+async function getSushiswapTopTokens() {
+  return await get("https://api.thegraph.com/subgraphs/name/sushiswap/matic-exchange", sushiswap_request)
 }
 
 
@@ -434,13 +461,173 @@ async function launchUniswap() {
 }
 
 
+// Program - Sushiswap
+async function launchSushiswap() {
+  let sushiswap_data_file = {}
+  let sushiswap_charts_file = {}
+  try {
+    sushiswap_data_file = require(path.join(dir_home, 'sushiswap-simple.json'))
+    sushiswap_charts_file = require(path.join(dir_home, 'sushiswap-charts.json'))
+  } catch(error) {
+    // console.log(error)
+  }
+
+  sushiswap_data = sushiswap_data_file
+  sushiswap_charts = sushiswap_charts_file
+
+
+  // get data from Sushiswap
+  const top = await getSushiswapTopTokens()
+
+
+
+  const time = Date.now()
+  const tokens = top.data ? top.data.tokens : []
+
+  const eth_price = top.data ? top.data.bundle.ethPrice : 0
+
+  tokens.forEach(token => {
+      const address = token.id
+      const symbol = token.symbol
+      const name = token.name
+      const price_ETH = token.derivedETH
+      const price = price_ETH * eth_price
+
+      // create Sushiswap list
+      sushiswap_list[address] = symbol
+
+
+      // update Sushiswap simple data
+      sushiswap_data[address] = {
+        s: symbol,
+        n: name,
+        p: price,
+        t: time
+      }
+
+      // update Sushiswap charts
+      //
+      if(sushiswap_charts[address]) {
+        if(time - sushiswap_charts[address].chart_often[sushiswap_charts[address].chart_often.length-1]['t'] > OFTEN) {
+          sushiswap_charts[address].chart_often.push({
+            t: time,
+            p: price,
+          })
+          sushiswap_charts[address].chart_often = sushiswap_charts[address].chart_often.slice(-HISTORY_SIZE)
+        }
+      } else {
+        sushiswap_charts[address] = {
+          s: symbol,
+          n: name,
+          chart_often: [{
+            t: time,
+            p: price,
+          }]
+        }
+      }
+      if(sushiswap_charts[address].chart_4h) {
+        if((time - sushiswap_charts[address].chart_4h[sushiswap_charts[address].chart_4h.length-1]['t']) > HOURS) {
+          sushiswap_charts[address].chart_4h.push({
+            t: time,
+            p: price,
+          })
+          sushiswap_charts[address].chart_4h = sushiswap_charts[address].chart_4h.slice(-HISTORY_SIZE)
+        }
+      } else {
+        sushiswap_charts[address].chart_4h = [{
+          t: time,
+          p: price,
+        }]
+      }
+      if(sushiswap_charts[address].chart_3d) {
+        if((time - sushiswap_charts[address].chart_3d[sushiswap_charts[address].chart_3d.length-1]['t']) > DAYS) {
+          sushiswap_charts[address].chart_3d.push({
+            t: time,
+            p: price,
+          })
+          sushiswap_charts[address].chart_3d = sushiswap_charts[address].chart_3d.slice(-HISTORY_SIZE)
+        }
+      } else {
+        sushiswap_charts[address].chart_3d = [{
+          t: time,
+          p: price,
+        }]
+      }
+      if(sushiswap_charts[address].chart_1w) {
+        if((time - sushiswap_charts[address].chart_1w[sushiswap_charts[address].chart_1w.length-1]['t']) > WEEK) {
+          sushiswap_charts[address].chart_1w.push({
+            t: time,
+            p: price,
+          })
+          sushiswap_charts[address].chart_1w = sushiswap_charts[address].chart_1w.slice(-HISTORY_SIZE)
+        }
+      } else {
+        sushiswap_charts[address].chart_1w = [{
+          t: time,
+          p: price,
+        }]
+      }
+  })
+
+
+  // build Top 25 list of Sushiswap
+  sushiswap_top = {}
+  if(tokens.length > 0) {
+    for (var i = 0; i < 25; i++) {
+      const token = tokens[i]
+      const address = token.id
+      const symbol = token.symbol
+      const name = token.name
+      const price_ETH = token.derivedETH
+      const price = price_ETH * eth_price
+
+      sushiswap_top[address] = {
+        s: symbol,
+        n: name,
+        p: price,
+        chart: sushiswap_charts[address].chart_often
+      }
+    }
+  }
+
+
+  /* Store files */
+
+  // Update the Sushiswap list
+  let pathFile = path.join(dir_home, 'sushiswap.json')
+  writeFile( pathFile, JSON.stringify( sushiswap_list ), "utf8", (err) => {
+    if (err) throw err;
+  });
+
+  // Update the Sushiswap top 25
+  pathFile = path.join(dir_home, 'sushiswap-top.json')
+  writeFile( pathFile, JSON.stringify( sushiswap_top ), "utf8", (err) => {
+    if (err) throw err;
+  });
+
+  // Update the Sushiswap simple data
+  pathFile = path.join(dir_home, 'sushiswap-simple.json')
+  writeFile( pathFile, JSON.stringify( sushiswap_data ), "utf8", (err) => {
+    if (err) throw err;
+  });
+
+  // Update the Sushiswap charts
+  pathFile = path.join(dir_home, 'sushiswap-charts.json')
+  writeFile( pathFile, JSON.stringify( sushiswap_charts ), "utf8", (err) => {
+    if (err) throw err;
+  });
+
+  // loop
+  setTimeout(function(){ launchSushiswap() }, OFTEN) // every 15 minutes
+}
+
+
 
 
 /* MAIN */
-setTimeout(function(){
-  launch()
-  launchUniswap()
-}, 2500)
+setTimeout(function(){ launch() }, 1500)
+setTimeout(function(){ launchUniswap() }, 2500)
+setTimeout(function(){ launchSushiswap() }, 3500)
 
 
 
@@ -464,6 +651,11 @@ app.get('/list/uniswap', (req, res) => res.json(uniswap_list))
 app.get('/top/uniswap', (req, res) => res.json(uniswap_top))
 app.get('/simple/uniswap', (req, res) => res.json(uniswap_data))
 app.get('/charts/uniswap', (req, res) => res.json(uniswap_charts))
+// Sushiswap URLs
+app.get('/list/sushiswap', (req, res) => res.json(sushiswap_list))
+app.get('/top/sushiswap', (req, res) => res.json(sushiswap_top))
+app.get('/simple/sushiswap', (req, res) => res.json(sushiswap_data))
+app.get('/charts/sushiswap', (req, res) => res.json(sushiswap_charts))
 
 app.listen(port, () => console.log(`Backend start at ${port}`))
 
