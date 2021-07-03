b8bac27a317253ff2c8b540fa1e98dee45f7e96e
Dorian Bayart <dorianbayart@users.noreply.github.com>
2021-07-03 18:57:33 +0200

    Commit back the needed ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  apps : [{
+    name: 'DexPairs_Back',
+    script: 'back.js',
+    exp_backoff_restart_delay: 100,
+    env_production: {
+      NODE_ENV: "production",
+      PORT: 3000,
+    }
+  }, {
+    name: 'DexPairs_Front',
+    script: 'front.js',
+    exp_backoff_restart_delay: 100,
+    instances: 2,
+    exec_mode: "cluster",
+    env_production: {
+      NODE_ENV: "production",
+      PORT: 3001,
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'dexpairs',
+      host : '185.212.226.82',
+      ref  : 'origin/main',
+      repo : 'git@github.com:dorianbayart/DexPairs.git',
+      path : '/home/dexpairs/prod',
+      'pre-deploy-local': '',
+      'post-deploy': 'npm install && pm2 startOrReload ecosystem.config.js --env production && pm2 save',
+      'pre-setup': 'pm2 install pm2-logrotate',
+      env_production: {
+        NODE_ENV: "production"
+      }
+    }
+  }
+};
