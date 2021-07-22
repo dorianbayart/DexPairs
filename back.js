0eae1adaafc941a15e1e63a8cc0b8ead126f5b31
Dorian Bayart <dorianbayart@users.noreply.github.com>
2021-07-22 16:11:11 +0200

    Update back.js

--- back.js
+++ back.js
@@ -3,7 +3,7 @@ const os = require('os')
 const path = require('path')
 const express = require('express')
 const fetch = require('node-fetch')
-const { writeFile } = require('fs')
+const { readFileSync, writeFile } = require('fs')
 
 
 
@@ -229,11 +229,11 @@ async function launch() {
   let pancakeswap_volume_file = {}
   try {
     //tokens_data_file = require(JSON.parse(path.join(dir_home, 'pancake-simple.json')))
-    tokens_data_file = fs.readFileSync(path.join(dir_home, 'pancake-simple.json'), 'utf8');
+    tokens_data_file = readFileSync(path.join(dir_home, 'pancake-simple.json'), 'utf8');
     //tokens_charts_file = require(JSON.parse(path.join(dir_home, 'pancake-charts.json')))
-    tokens_charts_file = fs.readFileSync(path.join(dir_home, 'pancake-charts.json'), 'utf8');
+    tokens_charts_file = readFileSync(path.join(dir_home, 'pancake-charts.json'), 'utf8');
     //pancakeswap_volume_file = require(JSON.parse(path.join(dir_home, 'pancake-volume.json')))
-    pancakeswap_volume_file = fs.readFileSync(path.join(dir_home, 'pancake-volume.json'), 'utf8');
+    pancakeswap_volume_file = readFileSync(path.join(dir_home, 'pancake-volume.json'), 'utf8');
 
     tokens_data = JSON.parse(tokens_data_file.toString());
     tokens_charts = JSON.parse(tokens_charts_file.toString());
