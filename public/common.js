fa4d375c223de3ba0e289b0e3d3d1dca45915a19
Dorian Bayart <dorianbayart@users.noreply.github.com>
2021-08-11 23:18:45 +0200

    Update common.js

--- public/common.js
+++ public/common.js
@@ -142,24 +142,44 @@ const minABI = [
          "name":"decimals",
          "outputs":[{"name":"","type":"uint8"}],
          "type":"function"
-       },
-       // tokenURI
-       {
-         "constant":true,
-         "inputs":[{"name":"_tokenId","type":"uint256"}],
-         "name":"tokenURI",
-         "outputs":[{"name":"","type":"string"}],
-         "type":"function"
-       },
-       // tokenOfOwnerByIndex
-       {
-         "constant":true,
-         "inputs":[{"name":"_owner","type":"address"}, {"name":"_index","type":"uint256"}],
-         "name":"tokenOfOwnerByIndex",
-         "outputs":[{"name":"tokenId","type":"uint256"}],
-         "type":"function"
        }
      ]
+
+const nftABI = [
+      // balanceOf
+      {
+        "constant":true,
+        "inputs":[{"name":"_owner","type":"address"}],
+        "name":"balanceOf",
+        "outputs":[{"name":"balance","type":"uint256"}],
+        "type":"function"
+      },
+      // decimals
+      {
+        "constant":true,
+        "inputs":[],
+        "name":"decimals",
+        "outputs":[{"name":"","type":"uint8"}],
+        "type":"function"
+      },
+      // tokenURI
+      {
+        "constant":true,
+        "inputs":[{"name":"_tokenId","type":"uint256"}],
+        "name":"tokenURI",
+        "outputs":[{"name":"","type":"string"}],
+        "type":"function"
+      },
+      // tokenOfOwnerByIndex
+      {
+        "constant":true,
+        "inputs":[{"name":"_owner","type":"address"}, {"name":"_index","type":"uint256"}],
+        "name":"tokenOfOwnerByIndex",
+        "outputs":[{"name":"tokenId","type":"uint256"}],
+        "type":"function"
+      }
+    ]
+
 
 let web3_ethereum = null
 let web3_polygon = null
