312ddfbf1d6333f9f8daddefd290530aefd95f77
Dorian Bayart <dorianbayart@users.noreply.github.com>
2021-08-17 01:30:10 +0200

    [BETA] Fix url of API requests

--- beta/public/common.js
+++ beta/public/common.js
@@ -22,7 +22,7 @@ const COLOR_THEMES = {
 
 /* Backend server */
 /* https://api.dexpairs.xyz or empty for localhost */
-const server = ''
+const server = 'https://api.dexpairs.xyz'
 const DOMAIN_NAME = 'DexPairs.xyz'
 
 const ALPHA_NUM = 'abcdefghijklmnopqrstuvwxyz0123456789-'
