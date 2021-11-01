f4cbce6dff122e9f8f6034618ca2ca5b16df4512
Dorian Bayart <dorianbayart@users.noreply.github.com>
2021-11-01 14:28:36 +0100

    Fix indentation

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,37 +1,37 @@
 module.exports = {
-  apps : [{
-    name: 'DexPairs_Back',
-    script: 'back.js',
-    exp_backoff_restart_delay: 100,
-    env_production: {
-      NODE_ENV: "production",
-      PORT: 3000,
-    }
-  }, {
-    name: 'DexPairs_Front',
-    script: 'front.js',
-    exp_backoff_restart_delay: 100,
-    instances: 1,
-    exec_mode: "cluster",
-    env_production: {
-      NODE_ENV: "production",
-      PORT: 3001,
-    }
-  }],
+	apps : [{
+		name: 'DexPairs_Back',
+		script: 'back.js',
+		exp_backoff_restart_delay: 100,
+		env_production: {
+			NODE_ENV: 'production',
+			PORT: 3000,
+		}
+	}, {
+		name: 'DexPairs_Front',
+		script: 'front.js',
+		exp_backoff_restart_delay: 100,
+		instances: 1,
+		exec_mode: 'cluster',
+		env_production: {
+			NODE_ENV: 'production',
+			PORT: 3001,
+		}
+	}],
 
-  deploy : {
-    production : {
-      user : 'dexpairs',
-      host : '185.212.226.82',
-      ref  : 'origin/main',
-      repo : 'git@github.com:dorianbayart/DexPairs.git',
-      path : '/home/dexpairs/prod',
-      'pre-deploy-local': '',
-      'post-deploy': 'npm install && pm2 startOrReload ecosystem.config.js --env production',
-      'pre-setup': 'pm2 install pm2-logrotate',
-      env_production: {
-        NODE_ENV: "production"
-      }
-    }
-  }
-};
+	deploy : {
+		production : {
+			user : 'dexpairs',
+			host : '185.212.226.82',
+			ref  : 'origin/main',
+			repo : 'git@github.com:dorianbayart/DexPairs.git',
+			path : '/home/dexpairs/prod',
+			'pre-deploy-local': '',
+			'post-deploy': 'npm install && pm2 startOrReload ecosystem.config.js --env production',
+			'pre-setup': 'pm2 install pm2-logrotate',
+			env_production: {
+				NODE_ENV: 'production'
+			}
+		}
+	}
+}
